75b52bbd1787fef4de84c27438652454346ddbb1
Eric Marrone <emarrone17@gmail.com>
2017-11-24 16:21:31 -0500

    Add exercise.rb

---
+++ exercise.rb
@@ -0,0 +1,58 @@
+ballots = [{1 => 'Smudge', 2 => 'Tigger', 3 => 'Simba'},
+           {1 => 'Bella', 2 => 'Lucky', 3 => 'Tigger'},
+           {1 => 'Bella', 2 => 'Boots', 3 => 'Smudge'},
+           {1 =>'Boots', 2 => 'Felix', 3 => 'Bella'},
+           {1 => 'Lucky', 2 => 'Felix', 3 => 'Bella'},
+           {1 => 'Smudge', 2 => 'Simba', 3 => 'Felix'}]
+
+
+
+
+#list of names
+
+names = []
+
+ballots.each do |ballot|
+  ballot_names = ballot.values
+  ballot_names.each do |name|
+    names << name
+  end
+end
+
+names.uniq!
+puts names
+
+names_hash = Hash[names.map {|v| [v,0]}]
+puts names_hash
+
+# invert = []
+# #
+# ballots.each do |ballot|
+#   reverse = ballot.invert
+#   invert << reverse
+# end
+
+# puts invert.inspect
+
+def tally(ballots, names)
+  names.each { |k, v| names[k] = 0 }
+  ballots.each do |ballot|
+    ballot.each do |key, value|
+      if key ==1
+        names[value] += 3
+      elsif key ==2
+        names[value] += 2
+      elsif key ==3
+        names[value] += 1
+      end
+    end
+  end
+end
+
+tally(ballots, names_hash)
+tally(ballots, names_hash)
+tally(ballots, names_hash)
+tally(ballots, names_hash)
+tally(ballots, names_hash)
+puts names_hash
+puts names_hash.sort_by(&:last)
